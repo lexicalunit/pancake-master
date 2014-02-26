736e2f9c574b82c132cdb6ab3b85684c8aeb92f2
lexicalunit <lexicalunit@lexicalunit.com>
2014-02-26 11:15:22 -0600

    Cleaning up code and adding documentation.

--- pcake.py
+++ pcake.py
@@ -9,6 +9,7 @@ import re
 import smtplib
 import urllib2
 
+
 from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -21,38 +22,51 @@ log.setLevel(logging.DEBUG)
 
 
 COMMASPACE = ', '
+DATETIME_FORMAT = '%A, %B %d, %Y - %I:%M%p'
+PANCAKE_MARKET = 0 # 0 is Austin's market id
+PICKLE_FILE = 'pcake.p'
 RECIPIENTS = [line.strip() for line in open('pcake.list')]
 SENDER = 'pancake-alerter'
-PICKLE_FILE = 'pcake.p'
 TEMPLATE_FILE = 'pcake.html'
-DATETIME_FORMAT = '%A, %B %d, %Y - %I:%M%p'
 
 
-def pancake_sort_key(pancake):
+def pancake_datetime(pancake):
+    """Returns a datetime object representing the show time of the given pancake."""
     timestamp = '{} - {}'.format(pancake['date'], pancake['time'])
+
+    # Alamo Drafthouse API returns times with a 'p' appended for PM, otherwise assume AM
     if timestamp.endswith('p'):
         timestamp = timestamp[:-1] + 'PM'
     else:
         timestamp = timestamp[:-1] + 'AM'
-    return pancake['film'], pancake['cinema'], datetime.strptime(timestamp, DATETIME_FORMAT)
+
+    return datetime.strptime(timestamp, DATETIME_FORMAT)
+
+
+def pancake_sort_key(pancake):
+    """Key to sort pancakes by film title, cinema location, and datetime of the show."""
+    return pancake['film'], pancake['cinema'], pancake_datetime(pancake)
 
 
 def pancake_html(pancakes):
+    """Returns an HTML string digest of the given pancakes."""
     pancakes = sorted(pancakes, key=pancake_sort_key)
 
-    def times_html(pancakes):
+    def pancake_times_html(pancakes):
+        """Returns a list of pancake times, as HTML, with hyperlink if currently on sale."""
         return ['<a href="{}">{}</a>'.format(p['url'], p['time']) if p['onsale'] else p['time'] for p in pancakes]
 
+    # things to group by
+    by_film_and_cinema = lambda p: (p['film_uid'], p['film'], p['cinema_url'], p['cinema'])
+    by_date = lambda p: p['date']
+
     content = ''
-    for film, date_times in groupby(pancakes, key=lambda p: (p['film'], p['film_uid'], p['cinema'], p['cinema_url'])):
-
-        content += '<h1><a href="https://drafthouse.com/uid/{film_uid}/">{film}</h1>\n'.format(film_uid=film[1], film=film[0])
-        content += '<h2><a href="{cinema_url}">{cinema}</a></h2>\n'.format(cinema_url=film[3], cinema=film[2])
+    for k, pancakes in groupby(pancakes, key=by_film_and_cinema):
+        content += '<h1><a href="https://drafthouse.com/uid/{}/">{}</h1>\n'.format(k[0], k[1])
+        content += '<h2><a href="{}">{}</a></h2>\n'.format(k[2], k[3])
         content += '<ul>\n'
-
-        for date, times in groupby(date_times, key=lambda p: p['date']):
-            content += '    <li>{} - {}</li>\n'.format(date, ', '.join(times_html(times)))
-
+        for k, pancakes in groupby(pancakes, key=by_date):
+            content += '    <li>{} - {}</li>\n'.format(k, ', '.join(pancake_times_html(pancakes)))
         content += '</ul>\n\n'
 
     template = open(TEMPLATE_FILE, 'r').read()
@@ -60,21 +74,27 @@ def pancake_html(pancakes):
 
 
 def pancake_text(pancakes):
+    """Returns a plain text digest of the given pancakes."""
     text = ''
     for pancake in sorted(pancakes, key=pancake_sort_key):
-        text += '{}\n{}\n{}\n{}\n{}'.format(
-            pancake['film']
-            , pancake['cinema']
-            , pancake['date']
-            , pancake['time']
-            , 'On sale now!' if pancake['onsale'] else 'Not on sale.'
-        )
+        params = {
+            pancake['film'],
+            pancake['cinema'],
+            pancake['date'],
+            pancake['time'],
+            'On sale now!' if pancake['onsale'] else 'Not on sale.',
+        }
+        text += '{}\n{}\n{}\n{}\n{}'.format(params)
         if pancake['onsale']:
             text += '\n{}'.format(pancake['url'])
         text += '\n\n'
 
 
 def notify(pancakes):
+    """Sends digest email(s) to RECIPIENTS given pancakes (no email sent if pancakes is empty)."""
+    if not pancakes:
+        return
+
     try:
         msg = MIMEMultipart('alternative')
         msg['Subject'] = 'Pancake Alert: {}'.format(datetime.strftime(datetime.now(), DATETIME_FORMAT))
@@ -93,32 +113,36 @@ def notify(pancakes):
         raise
 
 
-def json_data(data):   
+def parse_data(data):
+    """Parses out and returns the data from a Alamo Drafthouse API data response."""
     return json.loads(re.search('.*?({.*)\)', data).group(1))
 
 
 def query_cinemas(market_id):
+    """Queries the Alamo Drafthouse API for the list of cinemas in a given market."""
     try:
         r = urllib2.urlopen('https://d20ghz5p5t1zsc.cloudfront.net/adcshowtimeJson/marketsessions.aspx?callback=callback&date=20140223&marketid={:04.0f}'.format(market_id)).read()
+        data = parse_data(r)
     except Exception as e:
         log.error('market sessions fail: {}'.format(e))
         raise
 
-    data = json_data(r)
-
+    cinemas = []
     for cinema in data['Market']['Cinemas']:
-       yield int(cinema['CinemaId']), cinema['CinemaName'], cinema['CinemaURL']
+       cinemas.append((int(cinema['CinemaId']), cinema['CinemaName'], cinema['CinemaURL']))
+    return cinemas
 
 
 def query_pancakes(market_id):
+    """Queries the Alamo Drafthouse API for the list of pancakes in a given market."""
+    pancakes = []
     for cinema_id, cinema, cinema_url in query_cinemas(market_id):
         try:
             r = urllib2.urlopen('https://d20ghz5p5t1zsc.cloudfront.net/adcshowtimeJson/CinemaSessions.aspx?cinemaid={:04.0f}&callback=callback'.format(cinema_id)).read()
+            data = parse_data(r)
         except Exception as e:
             log.error('cinema sessions fail: {}'.format(e))
             raise
-
-        data = json_data(r)
 
         for date_data in data['Cinema']['Dates']:
             for film_data in date_data['Films']:
@@ -130,7 +154,7 @@ def query_pancakes(market_id):
 
                 for session_data in film_data['Sessions']:
                     onsale = session_data['SessionStatus'] == 'onsale'
-                    result = {
+                    pancake = {
                         'film': film.lstrip('Master Pancake: ').title(),
                         'film_uid': film_uid,
                         'cinema': cinema,
@@ -140,11 +164,13 @@ def query_pancakes(market_id):
                         'onsale': onsale,
                     }
                     if onsale:
-                        result['url'] = session_data['SessionSalesURL']
-                    yield result
+                        pancake['url'] = session_data['SessionSalesURL']
+                    pancakes.append(pancake)
+    return pancakes
 
 
 def pancake_key(pancake):
+    """Creates a unique id for a given pancake."""
     m = hashlib.md5()
     m.update(pancake['film'])
     m.update(pancake['cinema'])
@@ -153,17 +179,21 @@ def pancake_key(pancake):
     return m.hexdigest()
 
 
-def save(filename, object, bin=1):
+def save_database(db):
+    """Saves pancake database to disk."""
+    filename = PICKLE_FILE
     log.info('saving {}'.format(filename))
     try:
         with gzip.GzipFile(filename, 'wb') as f:
-            f.write(pickle.dumps(object, bin))
+            f.write(pickle.dumps(db, 1))
     except Exception as e:
         log.error('load failure: {}'.format(e))
         raise
 
 
-def load(filename):
+def load_database():
+    """Unpickles and decompresses the given file and returns the created object."""
+    filename = PICKLE_FILE
     log.info('loading {}'.format(filename))
     try:
         with gzip.GzipFile(filename, 'rb') as f:
@@ -179,31 +209,35 @@ def load(filename):
         raise
 
 
-def find_pancakes(db, market_id):
-    pancakes = []
-    for pancake in query_pancakes(market_id):
+def update_pancakes(db, pancakes):
+    """Updates the given database of pancakes given the list of all pancakes, returns list of updated pancakes."""
+    updated = []
+    for pancake in pancakes:
         key = pancake_key(pancake)
 
         if key in db and db[key]['onsale'] != pancake['onsale'] and pancake['onsale']:
-            pancakes.append(pancake)
+            updated.append(pancake)
         elif key not in db:
-            pancakes.append(pancake)
+            updated.append(pancake)
     
         db[key] = pancake
-    return pancakes
+    return updated
+
+
+def prune_database(db):
+    """Removes old pancakes from the database."""
+    # TODO: Go through the database checking to see if the pancakes are old, remove them if they are.
 
 
 if __name__ == '__main__':
     try:
-        db = load(PICKLE_FILE)
+        db = load_database()
     except:
+        # start new database if we couldn't load one
         db = {}
 
-    try:
-        pancakes = find_pancakes(db, 0) # 0 is Austin's market id
-        if pancakes:
-            notify(pancakes)
-    except Exception as e:
-        log.error('script fail: {}'.format(e))
-
-    save(PICKLE_FILE, db)
+    pancakes = query_pancakes(PANCAKE_MARKET)
+    updated = update_pancakes(db, pancakes)
+    notify(updated)
+    prune_database(db)
+    save_database(db)
