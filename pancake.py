11f436ea324e36b9c1fd494742eb3b69f2f6bab1
lexicalunit <lexicalunit@lexicalunit.com>
2014-03-05 21:57:57 -0500

    Added timezone information to pancake data, refactored date time code.

--- pancake.py
+++ pancake.py
@@ -17,6 +17,7 @@ from datetime import datetime, time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from itertools import groupby
+from pytz import timezone
 
 
 # setup logging to pancake.log and console
@@ -37,13 +38,17 @@ log.addHandler(ch)
 log.addHandler(fh)
 
 
-COMMASPACE = ', '
-DATETIME_FORMAT = '%A, %B %d, %Y - %I:%M%p'
 PANCAKE_MARKET = 0 # 0 is Austin's market id
-PICKLE_FILE = 'pancake.p'
-RECIPIENTS = []
-TEMPLATE_FILE = 'pancake.html'
+PANCAKE_TIMEZONE = timezone('US/Central') # Austin is US/Central
+
+PICKLE_FILE = 'pancake.pickle'
+RECIPIENTS_FILE = 'pancake.list'
 STYLESHEET_FILE = 'pancake.css'
+TEMPLATE_FILE = 'pancake.html'
+
+ALAMO_DATETIME_FORMAT = '%A, %B %d, %Y - %I:%M%p'
+DATE_FORMAT = '%A, %B %d, %Y'
+TIME_FORMAT = '%I:%M%p'
 TODAY = datetime.now()
 
 
@@ -53,22 +58,25 @@ class API(object):
     market_sessions_url = 'https://d20ghz5p5t1zsc.cloudfront.net/adcshowtimeJson/marketsessions.aspx'
 
 
-def pancake_datetime(pancake):
-    """Returns a datetime object representing the show time of the given pancake."""
-    timestamp = '{} - {}'.format(pancake['date'], pancake['time'])
+def date_string(dt):
+    """Returns a date string representation of the given datetime object."""
+    return datetime.strftime(dt, DATE_FORMAT)
 
-    # Alamo Drafthouse API returns times with a 'p' appended for PM, otherwise assume AM
-    if timestamp.endswith('p'):
-        timestamp = timestamp[:-1] + 'PM'
-    else:
-        timestamp = timestamp[:-1] + 'AM'
 
-    return datetime.strptime(timestamp, DATETIME_FORMAT)
+def time_string(dt):
+    """Returns a time string representation of the given datetime object."""
+    time = datetime.strftime(dt, TIME_FORMAT).lstrip('0')
+    return time[:-2] + 'p' if time.endswith('PM') else time[:-2]
+
+
+def datetime_string(dt):
+    """Returns a date and time string representation of the given datetime object."""
+    return date_string(dt) + ' - ' + time_string(dt)
 
 
 def pancake_sort_key(pancake):
     """Key to sort pancakes by film title, cinema location, and datetime of the show."""
-    return pancake['film'], pancake['cinema'], pancake_datetime(pancake)
+    return pancake['film'], pancake['cinema'], pancake['datetime']
 
 
 def pancake_html(pancakes):
@@ -77,19 +85,24 @@ def pancake_html(pancakes):
 
     def pancake_times_html(pancakes):
         """Returns a list of pancake times, as HTML, with hyperlink if currently on sale."""
-        return ['<a href="{}">{}</a>'.format(p['url'], p['time']) if p['onsale'] else p['time'] for p in pancakes]
+        return [
+            '<a href="{}">{}</a>'.format(p['url'], time_string(p['datetime']))
+            if p['onsale']
+            else time_string(p['datetime'])
+            for p in pancakes
+        ]
 
     # things to group by
     by_film_and_cinema = lambda p: (p['film_uid'], p['film'], p['cinema_url'], p['cinema'])
-    by_date = lambda p: p['date']
+    by_datetime = lambda p: p['datetime']
 
     content = u''
     for k, pancakes in groupby(pancakes, key=by_film_and_cinema):
         content += u'<h1><a href="https://drafthouse.com/uid/{}/">{}</h1>\n'.format(k[0], k[1])
         content += u'<h2><a href="{}">{}</a></h2>\n'.format(k[2], k[3])
         content += u'<ul>\n'
-        for k, pancakes in groupby(pancakes, key=by_date):
-            content += u'    <li>{} - {}</li>\n'.format(k, ', '.join(pancake_times_html(pancakes)))
+        for k, pancakes in groupby(pancakes, key=by_datetime):
+            content += u'    <li>{} - {}</li>\n'.format(datetime_string(k), ', '.join(pancake_times_html(pancakes)))
         content += u'</ul>\n\n'
 
     try:
@@ -108,8 +121,8 @@ def pancake_text(pancakes):
         params = (
             pancake['film'],
             pancake['cinema'],
-            pancake['date'],
-            pancake['time'],
+            date_string(pancake['datetime']),
+            time_string(pancake['datetime']),
             u'On sale now!' if pancake['onsale'] else u'Not on sale.',
         )
         text += u'{}\n{}\n{}\n{}\n{}'.format(*params)
@@ -119,15 +132,15 @@ def pancake_text(pancakes):
     return text
 
 
-def notify(pancakes):
-    """Sends digest email(s) to RECIPIENTS given pancakes (no email sent if pancakes is empty)."""
+def notify(pancakes, recipients):
+    """Sends digest email(s) to recipients given pancakes (no email sent if pancakes is empty)."""
     if not pancakes:
         return
 
     try:
         msg = MIMEMultipart('alternative')
-        msg['Subject'] = 'Pancake Master: {}'.format(datetime.strftime(datetime.now(), DATETIME_FORMAT))
-        msg['To'] = COMMASPACE.join(RECIPIENTS)
+        msg['Subject'] = 'Pancake Master: {}'.format(datetime_string(datetime.now()))
+        msg['To'] = ', '.join(recipients)
 
         plain = pancake_text(pancakes)        
         log.info(u'digest:\n{}'.format(plain))
@@ -135,12 +148,12 @@ def notify(pancakes):
         msg.attach(MIMEText(plain, 'plain', 'utf-8'))
         msg.attach(MIMEText(pancake_html(pancakes), 'html', 'utf-8'))
 
-        if RECIPIENTS:
-            msg['From'] = RECIPIENTS[0]
+        if recipients:
+            msg['From'] = recipients[0]
             s = smtplib.SMTP('localhost')
-            s.sendmail(msg['From'], RECIPIENTS, msg.as_string())
+            s.sendmail(msg['From'], recipients, msg.as_string())
             s.quit()
-            log.info('sent email(s) to {}'.format(COMMASPACE.join(RECIPIENTS)))
+            log.info('sent email(s) to {}'.format(', '.join(recipients)))
     except Exception as e:
         log.error('email fail: {}'.format(e))
         raise
@@ -154,7 +167,12 @@ def parse_data(data):
 def query_cinemas(market_id):
     """Queries the Alamo Drafthouse API for the list of cinemas in a given market."""
     try:
-        url = '{}?&date={}&marketid={:04.0f}&callback=callback'.format(API.market_sessions_url, datetime.strftime(TODAY, '%Y%m%d'), market_id)
+        params = {
+            'url': API.market_sessions_url,
+            'date': datetime.strftime(TODAY, '%Y%m%d'),
+            'market_id': market_id,
+        }
+        url = '{url}?&date={date}&marketid={market_id:04.0f}&callback=callback'.format(**params)
         r = urllib2.urlopen(url).read()
         data = parse_data(r)
     except Exception as e:
@@ -169,10 +187,24 @@ def query_cinemas(market_id):
 
 def query_pancakes(market_id):
     """Queries the Alamo Drafthouse API for the list of pancakes in a given market."""
+
+    def pancake_datetime(date_str, time_str):
+        """Returns a datetime object representing the show time of the given pancake."""
+        timestamp = '{} - {}'.format(date_str, time_str)
+
+        # Alamo Drafthouse API returns times with a 'p' appended for PM, otherwise assume AM
+        timestamp = timestamp[:-1] + 'PM' if timestamp.endswith('p') else timestamp[:-1] + 'AM'
+
+        return PANCAKE_TIMEZONE.localize(datetime.strptime(timestamp, ALAMO_DATETIME_FORMAT))
+
     pancakes = []
     for cinema_id, cinema, cinema_url in query_cinemas(market_id):
         try:
-            url = '{}?cinemaid={:04.0f}&callback=callback'.format(API.cinema_sessions_url, cinema_id)
+            params = {
+                'url': API.cinema_sessions_url,
+                'cinema_id': cinema_id,
+            }
+            url = '{url}?cinemaid={cinema_id:04.0f}&callback=callback'.format(**params)
             r = urllib2.urlopen(url).read()
             data = parse_data(r)
         except Exception as e:
@@ -192,10 +224,10 @@ def query_pancakes(market_id):
                     pancake = {
                         'film': string.capwords(film.lstrip('Master Pancake: ').lower()),
                         'film_uid': film_uid,
+                        'url': None,
                         'cinema': cinema,
                         'cinema_url': cinema_url,
-                        'date': date_data['Date'],
-                        'time': session_data['SessionTime'],
+                        'datetime': pancake_datetime(date_data['Date'], session_data['SessionTime']),
                         'onsale': onsale,
                     }
                     if onsale:
@@ -209,8 +241,7 @@ def pancake_key(pancake):
     m = hashlib.md5()
     m.update(pancake['film'].encode('utf-8'))
     m.update(pancake['cinema'].encode('utf-8'))
-    m.update(pancake['date'].encode('utf-8'))
-    m.update(pancake['time'].encode('utf-8'))
+    m.update(datetime_string(pancake['datetime']).encode('utf-8'))
     return m.hexdigest()
 
 
@@ -262,7 +293,7 @@ def update_pancakes(db, pancakes):
 def prune_database(db):
     """Removes old pancakes from the database."""
     for key, pancake in db.items():
-        if pancake_datetime(pancake).date() < TODAY.date():
+        if pancake['datetime'].date() < TODAY.date():
             del db[key]
 
 if __name__ == '__main__':
@@ -273,14 +304,14 @@ if __name__ == '__main__':
         db = {}
 
     try:
-        RECIPIENTS = [line.strip() for line in open('pancake.list')]
+        recipients = [line.strip() for line in open(RECIPIENTS_FILE)]
     except:
         log.warn('no email recipients found, not sending email notifications...')
 
     try:
         pancakes = query_pancakes(PANCAKE_MARKET)
         updated = update_pancakes(db, pancakes)
-        notify(updated)
+        notify(updated, recipients)
         prune_database(db)
         save_database(db)
     except:
