f50ab00c04b5c0454fbc1c215eb9f89baac90f70
Amy Troschinetz <amy@lexicalunit.com>
2014-03-23 19:45:57 -0500

    Make deployment script work from anywhere.

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,18 @@
 import logging
+import os
 import yaml
 
-from fabric.api import lcd, env, task
+from fabric.api import lcd, env, task, local
 from fabric.contrib.project import rsync_project
 
 
 logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger()
 
+log = logging.getLogger()
+repo_root = local('git rev-parse --show-toplevel', capture=True)
 
 try:
-    conf = yaml.load(open('deploy.yaml', 'rb').read())
+    conf = yaml.load(open(os.path.join(repo_root, 'deploy.yaml'), 'rb').read())
 except:
     log.exception('error: unable to read deply.yaml config file:')
 
@@ -20,9 +22,10 @@ env.hosts = ['{}@{}:22'.format(env.user, host) for host in conf['hosts']]
 
 def deploy_project(local_dir, remote_dir, exclusions=[]):
     """Deploy the entire project at local_dir to remote_dir, excluding the given paths."""
-    with lcd(local_dir):
-        rsync_project(remote_dir=remote_dir, local_dir='.', exclude=exclusions)
-    rsync_project(remote_dir=remote_dir, local_dir='resources', exclude=exclusions, delete=True)
+    with lcd(repo_root):
+        with lcd(local_dir):
+            rsync_project(remote_dir=remote_dir, local_dir='.', exclude=exclusions)
+        rsync_project(remote_dir=remote_dir, local_dir='resources', exclude=exclusions, delete=True)
 
 
 @task
